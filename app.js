573e77bfbe2d02fd1f34eeaac9edbd1664f39c74
Cathy0127 <140610874+Cathy0127@users.noreply.github.com>
2023-07-26 12:22:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_sgfhvi3", "template_8fkbsrk", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
